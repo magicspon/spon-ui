2c1012553843c1bb6b182af35a9fbf4335a7d598
Dave Stockley <magicspon@gmail.com>
2018-09-19 00:16:20 +0100

    remove dist from srcga .

--- src/dist/js/6.bundle.1537312226028.js
+++
@@ -1,8 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[6],{7:
-/*!***************************!*\
-  !*** ./ui/VideoPlayer.js ***!
-  \***************************/
-/*! exports provided: default */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is referenced from these modules with unsupported syntax: ./ui lazy ^\.\/.*$ namespace object (referenced with context element) */function(n,t,e){"use strict";e.r(t),e.d(t,"default",function(){return a});var i=e(/*! plyr */25),o=e.n(i);function r(n,t,e){return t in n?Object.defineProperty(n,t,{value:e,enumerable:!0,configurable:!0,writable:!0}):n[t]=e,n}var a=function n(t){var e=this;!function(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(this,n),r(this,"mount",function(){}),r(this,"unmount",function(){e.player.destroy()}),this.$el=t,this.player=new o.a(this.$el)};n.hot.accept(function(n){n&&console.error(n)})}}]);
-//# sourceMappingURL=6.bundle.1537312226028.js.map
