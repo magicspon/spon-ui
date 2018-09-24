3179f7bf5d3c905191213ef3701fbfe77e5ebec2
Dave Stockley <magicspon@gmail.com>
2018-09-24 21:54:51 +0100

    added some filters/functions to twig enginre

--- scripts/fractal/engine.js
+++ scripts/fractal/engine.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const mdAbbr = require('markdown-it-abbr')
 const mdFootnote = require('markdown-it-footnote')
 const md = require('markdown-it')({
@@ -7,10 +9,13 @@ const md = require('markdown-it')({
 })
 	.use(mdAbbr)
 	.use(mdFootnote)
+
+const { getPublicPath } = require('../utils/paths')
 // const fs = require('fs')
 // const path = require('path')
 
-const stripUnits = value => Number((value || '').toString().replace(/[^\d\.-]/gi, '')) || null
+const stripUnits = value =>
+	Number((value || '').toString().replace(/[^\d\.-]/gi, '')) || null
 
 module.exports = function templateEngine(stamp) {
 	return {
@@ -24,15 +29,31 @@ module.exports = function templateEngine(stamp) {
 			rem2px(str) {
 				return `${stripUnits(str) * 16}px`
 			},
-			json_encode(str) {
-				return JSON.stringify(str, null, 2)
-			}
+			json_encode: str => JSON.stringify(str, null, 2),
+
+			// TO DO... implement these filters
+			// https://docs.craftcms.com/v3/dev/filters.html
+			camel: str => str,
+			kebab: str => str,
+			lcfirst: str => str,
+			snake: str => str,
+			ucfirst: str => str,
+
+			values: arr => arr,
+			without: arr => arr
 		},
 		functions: {
 			getStamp() {
 				return {
 					stamp
 				}
+			},
+
+			// craftcms form function
+			csrfInput() {},
+
+			source(src) {
+				return fs.readFileSync(getPublicPath(src), 'utf8')
 			}
 		}
 	}
