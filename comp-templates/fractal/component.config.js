4242c1a6aed085cbe82ebf4a565df28b7dcfd3bc
Dave Stockley <magicspon@gmail.com>
2018-10-16 01:20:14 +0100

    sample config

--- comp-templates/fractal/component.config.js
+++ comp-templates/fractal/component.config.js
@@ -5,3 +5,27 @@ module.exports = {
 
 	variants: []
 }
+
+/*
+
+sample context
+
+{
+	heading: 'This is a heading',
+
+	body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas optio illo est necessitatibus ex explicabo!</p>',
+
+	image: {
+		src: '/dist/img/face.jpg',
+		alt: 'Picture of a face',
+		width: '',
+		height: ''
+	},
+
+	link: {
+		href: '#0',
+		text: 'Read more'
+	},
+
+}
+*/
